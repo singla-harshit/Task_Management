9671ab653811355b53eadc8f876a937bfd35d32f
Harshit-0685 <harshitsinglaofficial23@gmail.com>
2025-03-17 16:58:21 +0530

    Added Database Connection

---
+++ backend/db/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config({path:'../.env'});
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(`${process.env.DB_URI}/${process.env.DB_NAME}`)
+        console.log("MongoDB Connected")
+    }
+    catch(err){
+        console.log("Error Connecting MongoDB : ",err);
+        process.exit(1);
+    }
+}
+
+
+module.exports = {connectDB}
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { connectDB } = require('./db');
+const port = process.env.PORT || 5000   ;
+
+connectDB()
 
 app.get('/',(req,res)=>{
     res.end("Server is running");
 })
+
+
+
 
 app.listen(port , ()=>{
     console.log(`server is running at http://localhost:${port}`);
